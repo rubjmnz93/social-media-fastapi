edc0d25729744b1e4964eba8ddd2962a447f1af3
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2024-06-18 13:02:05 +0200

    Updated code to work with PostgreSQL

--- socialmediaapi/database.py
+++ socialmediaapi/database.py
@@ -39,11 +39,12 @@ users_table = sqlalchemy.Table(
     sqlalchemy.Column("confirmed", sqlalchemy.Boolean, default=False),
 )
 
-engine = sqlalchemy.create_engine(
-    config.DATABASE_URL, connect_args={"check_same_thread": False}
-)
+connect_args = {"check_same_thread": False} if "sqlite" in config.DATABASE_URL else {}
+engine = sqlalchemy.create_engine(config.DATABASE_URL, connect_args=connect_args)
 
 metadata.create_all(engine)
+
+db_args = {"min_size": 1, "max_size": 3} if "sqlite" in config.DATABASE_URL else {}
 database = databases.Database(
-    config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK
+    config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK, **db_args
 )
